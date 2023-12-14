3cb3a07ea7a827b6343aa39089f671ed3980ab0d
ashishBendale777 <36222108+ashishBendale777@users.noreply.github.com>
2023-12-14 16:19:31 +0530

    inage

--- Server.js
+++ Server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require("mongoose")
 const bodyparser = require("body-parser")
 const cors = require('cors')
+const multer = require('multer')
+const path = require('path')
 
 //create server
 const server = express()
@@ -18,6 +20,40 @@ mongoose.connect("mongodb://127.0.0.1:27017/demo-db", {
 }).catch((err) => {
     console.log("DB Not Connected")
 });
+
+
+//storing image
+const fileStorageConfig = multer.diskStorage({
+    destination: "Uploads",
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname))
+    }
+})
+
+//upload config
+const fileUploadConfig = multer({
+    storage: fileStorageConfig,
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(png|jpg|jpeg|pdf)$/)) {
+            return cb(new Error("Please Upload Correct File"))
+        }
+        cb(undefined, true)
+    }
+})
+
+//upload image route
+server.post("/uploadfile",
+    fileUploadConfig.single('image'), (req, res) => {
+        res.status(200)
+            .json({
+                filepath: "/images/".concat(req.file.filename)
+                , uploaded: true
+            })
+    },
+    (err, req, res, next) => {
+        res.status(400).send({ error: err.message })
+    }
+)
 
 
 server.get('/', (req, res) => {
